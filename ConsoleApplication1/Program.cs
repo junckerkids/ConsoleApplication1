87b6b968306dc94c151d6cdbcdd5e4b4fc77a227
Peter Kristensen <ptx@cs.au.dk>
2013-08-20 11:01:11 +0200

    Mere ting!

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        // Dette program er fjollet
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hvad er dit navn? ");
+            string navn = Console.ReadLine();
+
+            Console.WriteLine("Du er: ");
+            Console.WriteLine(navn);
+
+            /*
+             * Opgave 1a
+             * 
+             * Udvid dette program til at spoerge om navn, alder og 
+             * Naar alle ting er indtastet skal programmet skrive det tilbage til brugeren!
+             */
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ README.txt
@@ -0,0 +1,11 @@
+Opgave 1:
+
+Laes og forstaa Program.cs.
+I filen er der en kommentar der indholder en lille opgave, lav den!
+
+
+Opgave 2:
+
+Lav et nyt program der taeller ned fra 5.
+(brug en for-loekke og udskriv hvert tal paa en ny linje)
+Hint: soeg paa netter efter hvordan et tal konverteres til tekst.
